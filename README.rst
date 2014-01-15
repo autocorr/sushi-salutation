004d441186a294c43d8dc6da9391539d30bda37a
Brian Svoboda <bones253@gmail.com>
2014-01-14 19:21:58 -0700

    First commit

---
+++ README.rst
@@ -0,0 +1,17 @@
+Sushi Salutations!
+==================
+Why hello there california-roll-compatriots and aji-aesthetes.  This is a small
+module written to make alliterative salutations for, among other things, sushi
+related emails. The glossary at eatsushi.com was helpful in gathering a large
+number of terms. Contributions are welcome, please add more phrases if they
+come to mind -- GitHub's interface makes it quite easy to edit the files in the
+browser.
+
+Example
+-------
+See it in action:
+
+    >>> from sushi_salutations import get_sushi_salutations
+    >>> get_sushi_salutations(lines=3)
+        ['nori-noshers', 'ebi-enjoyers', 'engawa-experts']
+
---
+++ sushi_salutation.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import random
+import string
+
+
+__all__ = ['SushiSalutation']
+
+FOOD_FILEN = 'food_types.txt'
+RECIP_FILEN = 'recipients.txt'
+
+
+class NoMatchError(Exception):
+    def __init__(self):
+        pass
+
+
+class Items(object):
+    def __init__(self, filen):
+        self.filen = filen
+        self._get_entries()
+
+    def _get_entries(self):
+        with open(self.filen, 'r') as handle:
+            self.entries = self._clean_lines(handle)
+        self.valid_start_letters = set(s[0] for s in self.entries)
+
+    @staticmethod
+    def _clean_lines(handle):
+        # strip new line characters
+        entries = [s.strip() for s in handle.readlines()]
+        # remove empty lines
+        entries = filter(None, entries)
+        return entries
+
+    def select_on_letter(self, letter):
+        assert letter in string.ascii_lowercase
+        if letter not in self.valid_start_letters:
+            raise NoMatchError
+        return [s for s in self.entries if s.startswith(letter)]
+
+
+class SushiSalutation(object):
+    """
+    Randomly chose sushi related salutations.
+
+    Attributes
+    ----------
+    foods : Items
+    recips : Items
+    saluts : list
+        Salutations after `get_saluts` has been run
+
+    Example
+    -------
+    >>> sushi = SushiSalutation(lines=5)
+    >>> sushi.get_saluts()
+    """
+    foods = Items(filen=FOOD_FILEN)
+    recips = Items(filen=RECIP_FILEN)
+
+    def __init__(self, lines):
+        """
+        Parameters
+        ----------
+        lines : number
+            Number of salutations to print
+        """
+        self.lines = lines
+        self.saluts = []
+
+    @staticmethod
+    def _get_rand_letter():
+        return random.choice(string.ascii_lowercase)
+
+    def get_saluts(self):
+        if self.saluts:
+            self.saluts = []
+        while len(self.saluts) < self.lines:
+            try:
+                letter = self._get_rand_letter()
+                l_foods = self.foods.select_on_letter(letter=letter)
+                l_recip = self.recips.select_on_letter(letter=letter)
+            except NoMatchError:
+                pass
+            else:
+                food = random.choice(l_foods)
+                recip = random.choice(l_recip)
+                salut = '-'.join([food, recip])
+                self.saluts.append(salut)
+        return self.saluts
+
+
+def get_sushi_salutations(lines):
+    ss = SushiSalutation(lines=lines)
+    return ss.get_saluts()
+
+
